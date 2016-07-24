6dcfa2bf66edad5d5355e6e8ac4be967abc23aa4
wuzhaoshuai007086 <396181651@qq.com>
2016-07-24 11:47:13 +0800

    111

---
+++ js/index.js
@@ -0,0 +1,29 @@
+$(function(){
+
+    $('.container').fullpage({
+        onLeave: function(index, nextIndex, direction){
+            $('.in-left').attr("id","");
+            $('.in-right').attr("id","");
+            $('.in-top').attr("id","");
+            $('.in-bottom').attr("id","");
+            $('.in-top2').attr("id","");
+
+        },
+        afterLoad: function(anchorLink, index){
+            $('.in-left').attr("id","active");
+            $('.in-right').attr("id","active2");
+            $('.in-top').attr("id","active4");
+            $('.in-bottom').attr("id","active3");
+            $('.in-top2').attr("id","active5");
+
+        },
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5', 'page6'],
+        menu: '#menu'
+
+    })
+    $(".downs").click(function(){
+    	 $.fn.fullpage.moveSectionDown();
+    })
+
+
+}); 
